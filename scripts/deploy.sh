17131afe6dd1eb7766fb898713e4949413d75ca6
rajnish.malik <rajnish.malik@sunrun.com>
2020-06-23 14:11:39 -0700

    Introduced shaded

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 # Script to do a maven build and submit a job to Spark cluster
-JAR_NAME=storagetest-0.0.1-jar-with-dependencies.jar
+JAR_NAME=original-storagetest-0.0.1-shaded.jar
 GCS_JAR_ARTIFACT_BUCKET=gs://dataproc-temp-us-central1-21673414111-z46je66v/artifacts/scanseq
 GCS_BUCKET_DATA_OUT=gs://dataproc-temp-us-central1-21673414111-z46je66v/sampledata/out3
 REGION_NAME=us-central1
 CLUSTER_NAME=cluster-001a
 
-mvn package -DskipTests=true
+mvn clean package -DskipTests=true
 
 gsutil rm $GCS_JAR_ARTIFACT_BUCKET/$JAR_NAME
 gsutil cp ./target/$JAR_NAME $GCS_JAR_ARTIFACT_BUCKET
